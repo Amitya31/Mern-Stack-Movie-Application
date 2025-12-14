9db759935eedf7848750d1d14da22fedb7824501
Amitya31 <amitya1576@gmail.com>
2025-12-14 08:25:17 +0530

    queue and worker logic completed

--- backend/src/queues/movie.queue.ts
+++ backend/src/queues/movie.queue.ts
@@ -3,4 +3,13 @@ import connection from "../config/redis.js";
 
 export const movieQueue = new Queue("movie-import", {
   connection,
+  defaultJobOptions: {
+    attempts: 3,
+    backoff: {
+      type: "exponential",
+      delay: 3000,
+    },
+    removeOnComplete: true,
+    removeOnFail: false,
+  },
 });
